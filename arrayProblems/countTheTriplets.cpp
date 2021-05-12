90e16558084e24030fdab01ad592e3edcefa2b4f
ObliviousParadigm <adarshsdda@gmail.com>
2021-05-13 03:12:04 +0530

    Day 6 Optimized the code but it still gives TLE error

--- arrayProblems/countTheTriplets.cpp
+++ arrayProblems/countTheTriplets.cpp
@@ -8,41 +8,40 @@ public:
     int countTriplet(int arr[], int n)
     {
         sort(arr, arr + n);
-        int i = 0, j = 0, k = 0;
+        int i = 0, j = 0;
+        vector<int> s;
         int count = 0;
 
         for (i = 0; i < (n - 2); ++i)
         {
             for (j = i + 1; j < (n - 1); ++j)
             {
-                for (k = j + 1; k < n; ++k)
-                {
-                    if ((arr[i] + arr[j]) == arr[k])
-                    {
-                        count += 1;
-                        break;
-                    }
-                }
+                s.push_back(arr[i] + arr[j]);
             }
         }
+        
+        for (auto it = s.begin(); it != s.end(); ++it)
+        {
+            auto f = find(arr, arr+n, *it);
+            if (f != (arr+n))
+            {
+                count += 1;
+            }
+        }
+
         return count;
     }
 };
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
+    int n;
+    cin >> n;
 
-        int arr[n];
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-        Solution ob;
-        cout << ob.countTriplet(arr, n) << "\n";
-    }
+    Solution ob;
+    cout << ob.countTriplet(arr, n) << "\n";
 }
